e8472d0a9f2d4ea28477cc3978f42fe2a064deea
pastike <development@pastike.com>
2022-12-14 16:34:14 +0300

    init

--- src/app/index.jsx
+++
@@ -1,9 +0,0 @@
-import Routing from '../pages'
-import { withProviders } from './providers'
-import './style.css'
-
-const App = () => {
-  return <Routing />
-}
-
-export default withProviders(App)
--- src/app/providers/index.js
+++
@@ -1,5 +0,0 @@
-import compose from 'compose-function'
-
-import { ViewportSize } from './viewport-size'
-
-export const withProviders = compose(ViewportSize)
